5a75f186b9069808ce864bcd8b553decd5ad69aa
paolaleiteadv-sys <paola.leite.adv@gmail.com>
2026-07-12 13:27:51 +0100

    Delete src directory

--- src/components/WhatsApp.jsx
+++
@@ -1,4 +0,0 @@
-import { WHATSAPP_URL } from "../config";
-export default function WhatsApp() {
-  return <a className="whatsapp" href={WHATSAPP_URL} target="_blank" rel="noreferrer">WhatsApp</a>;
-}
